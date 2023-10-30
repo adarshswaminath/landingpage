caa3a4d39df9af1cf3c09c8be6627ca6b6fb1524
adarshswamianth <adarshswaminath7@gmail.com>
2023-10-30 21:24:54 +0530

    Feat: framer motion load animation in hero

--- src/Components/Hero/SecondHero.jsx
+++ src/Components/Hero/SecondHero.jsx
@@ -1,22 +1,35 @@
-import {AiOutlineInstagram,AiOutlineFacebook} from "react-icons/ai"
-import {PiTelegramLogoDuotone} from "react-icons/pi"
-import RightSquareArrow from "./RightSquareArrow";
+import React from "react";
+import { motion } from "framer-motion";
 import LeftSquareArrow from "./LeftSquareArrow";
-function SecondHero({Increment,Decrement}) {
+import RightSquareArrow from "./RightSquareArrow";
+
+function SecondHero({ Increment, Decrement }) {
   return (
-    <div className="min-h-screen bg-gradient-to-r from-[#c5e1f6] to-[#eeb0bf] rounded-t-[25px]">
+    <motion.div
+    initial={{ x: 1000 }}  // Initial position (off-screen to the left)
+    animate={{ x: 0 }}       // Animate to the original position
+      className="min-h-screen bg-gradient-to-r from-[#c5e1f6] to-[#eeb0bf] rounded-t-[25px]"
+    >
       <section>
         <div className="grid lg:flex lg:justify-between max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
-          <button onClick={Decrement} className="mt-0 lg:col-span-5 lg:flex">
-           <LeftSquareArrow/>
-          </button>
+          <motion.button
+            onClick={Decrement}
+            className="mt-0 lg:col-span-5 lg:flex"
+            whileHover={{ scale: 1.1 }}
+          >
+            <LeftSquareArrow />
+          </motion.button>
           <h1 className="text-center text-4xl">Hello Second Hero</h1>
-          <button onClick={Increment} className="mt-0 lg:col-span-5 lg:flex">
-            <RightSquareArrow/>
-          </button>
+          <motion.button
+            onClick={Increment}
+            className="mt-0 lg:col-span-5 lg:flex"
+            whileHover={{ scale: 1.1 }}
+          >
+            <RightSquareArrow />
+          </motion.button>
         </div>
       </section>
-    </div>
+    </motion.div>
   );
 }
 
--- src/Components/Hero/ThirdHero.jsx
+++ src/Components/Hero/ThirdHero.jsx
@@ -1,19 +1,25 @@
-import {AiOutlineInstagram,AiOutlineFacebook} from "react-icons/ai"
-import {PiTelegramLogoDuotone} from "react-icons/pi"
+import { motion } from "framer-motion";
+import { AiOutlineInstagram, AiOutlineFacebook } from "react-icons/ai";
+import { PiTelegramLogoDuotone } from "react-icons/pi";
 import RightSquareArrow from "./RightSquareArrow";
 import LeftSquareArrow from "./LeftSquareArrow";
-function ThirdHero({Decrement}) {
+
+function ThirdHero({ Decrement }) {
   return (
-    <div className="min-h-screen bg-gradient-to-r from-[#c5e1f6] to-[#eeb0bf] rounded-t-[25px]">
+    <motion.div
+      initial={{ x: 1000 }}  // Initial position (off-screen to the left)
+      animate={{ x: 0 }}       // Animate to the original position
+      className="min-h-screen bg-gradient-to-r from-[#c5e1f6] to-[#eeb0bf] rounded-t-[25px]"
+    >
       <section>
         <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
           <button onClick={Decrement} className="mt-4 lg:mt-0 lg:col-span-5 lg:flex">
-            <LeftSquareArrow/>
+            <LeftSquareArrow />
           </button>
           <h1 className="text-center text-4xl">Third Hero</h1>
         </div>
       </section>
-    </div>
+    </motion.div>
   );
 }
 
