b52d9e56c9300548c9e95d898eb539f21b6821c2
adarshswamianth <adarshswaminath7@gmail.com>
2023-11-05 12:32:21 +0530

    feat:Footer added

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import FooterService from './FooterService'
+import FooterContent from './FooterContent'
+import FooterBottom from './FooterBottom'
 
 function Footer() {
   return (
     <div className='py-12'>
         <FooterService/>
+        <FooterContent/>
+        <hr className='border-gray-600 mt-4 mb-4'/>
+        <FooterBottom/>
     </div>
   )
 }
